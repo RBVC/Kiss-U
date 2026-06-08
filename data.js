773d1310699575ce18004af3602b581d87503b9e
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-08 22:43:48 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const memberData = {
+    "1chika": {
+        name: "1chika",
+        birthday: "1月5日 (22歳)",
+        position: "メインボーカル",
+        image: "./images/1chika.png"
+    },
+    "3tuba": {
+        name: "3tuba",
+        birthday: "3月8日 (18歳)",
+        position: "マンネ / メインダンサー / メインラッパー",
+        image: "./images/3tuba.png"
+    },
+    "5ma": {
+        name: "5ma",
+        birthday: "5月20日 (25歳)",
+        position: "リーダー / リードボーカル",
+        image: "./images/5ma.png"
+    },
+    "na7o": {
+        name: "na7o",
+        birthday: "7月14日 (23歳)",
+        position: "リードボーカル / リードラッパー",
+        image: "./images/na7o.png"
+    },
+    "ku9": {
+        name: "ku9",
+        birthday: "9月9日 (19歳)",
+        position: "サブボーカル / サブラッパー",
+        image: "./images/ku9.png"
+    }
+};
