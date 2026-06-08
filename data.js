37b196bb71ecc64dda1689f3db61f0f1c8a2e08f
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-09 00:27:31 +0900

    up

--- data.js
+++ data.js
@@ -1,37 +1,20 @@
 const memberData = {
-    "1chika": {
-        codename: "1chika",
-        name: "イチカ・ハイビスカス",
-        birthday: "1月5日 (22歳)",
-        position: "メインボーカル",
-        image: "./images/1chika.png"
-    },
-    "3tuba": {
-        codename: "3tuba",
-        name: "ミツハ・トリフォリウム",
-        birthday: "3月8日 (18歳)",
-        position: "マンネ / メインダンサー / メインラッパー",
-        image: "./images/3tuba.png"
-    },
-    "5ma": {
-        codename: "5ma",
-        name: "ゴマ・フリージア",
-        birthday: "5月20日 (25歳)",
-        position: "リーダー / リードボーカル",
-        image: "./images/5ma.png"
-    },
-    "na7o": {
-        codename: "na7o",
-        name: "ナナオ・ピクシーデライト",
-        birthday: "7月14日 (23歳)",
-        position: "リードボーカル / リードラッパー",
-        image: "./images/na7o.png"
-    },
-    "ku9": {
-        codename: "ku9",
-        name: "クク・ブロッサム",
-        birthday: "9月9日 (19歳)",
-        position: "サブボーカル / サブラッパー",
-        image: "./images/ku9.png"
+    "1chika": { codename: "1chika", name: "イチカ・ハイビスカス", birthday: "1月5日 (22歳)", position: "メインボーカル", image: "./images/1chika.png" },
+    "3tuba": { codename: "3tuba", name: "ミツハ・トリフォリウム", birthday: "3月8日 (18歳)", position: "マンネ / メインダンサー / メインラッパー", image: "./images/3tuba.png" },
+    "5ma": { codename: "5ma", name: "ゴマ・フリージア", birthday: "5月20日 (25歳)", position: "リーダー / リードボーカル", image: "./images/5ma.png" },
+    "na7o": { codename: "na7o", name: "ナナオ・ピクシーデライト", birthday: "7月14日 (23歳)", position: "リードボーカル / リードラッパー", image: "./images/na7o.png" },
+    "ku9": { codename: "ku9", name: "クク・ブロッサム", birthday: "9月9日 (19歳)", position: "サブボーカル / サブラッパー", image: "./images/ku9.png" }
+};
+
+const discoData = {
+    "fly-by": {
+        title: "1st EP \"FLY-BY\"",
+        release: "2026.06.09",
+        jacket: "https://images.genius.com/a16644bd336d84437e195572f9ae309f.1000x1000x1.png",
+        tracks: [
+            { num: "01", name: "Yum", file: "./music/fly-by/01_yum.m4a" },
+            { num: "02", name: "MVSK", file: "./music/fly-by/02_mvsk.m4a" },
+            { num: "03", name: "Bubble Gum", file: "./music/fly-by/03_bubble_gum.m4a" }
+        ]
     }
 };
