dd321d2fd8c1e9d4bb37d37980e458661092fe92
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-08 22:58:06 +0900

    up

--- data.js
+++ data.js
@@ -1,30 +1,35 @@
 const memberData = {
     "1chika": {
-        name: "1chika",
+        codename: "1chika",
+        name: "イチカ・ハイビスカス",
         birthday: "1月5日 (22歳)",
         position: "メインボーカル",
         image: "./images/1chika.png"
     },
     "3tuba": {
-        name: "3tuba",
+        codename: "3tuba",
+        name: "ミツハ・トリフォリウム",
         birthday: "3月8日 (18歳)",
         position: "マンネ / メインダンサー / メインラッパー",
         image: "./images/3tuba.png"
     },
     "5ma": {
-        name: "5ma",
+        codename: "5ma",
+        name: "ゴマ・フリージア",
         birthday: "5月20日 (25歳)",
         position: "リーダー / リードボーカル",
         image: "./images/5ma.png"
     },
     "na7o": {
-        name: "na7o",
+        codename: "na7o",
+        name: "ナナオ・ピクシーデライト",
         birthday: "7月14日 (23歳)",
         position: "リードボーカル / リードラッパー",
         image: "./images/na7o.png"
     },
     "ku9": {
-        name: "ku9",
+        codename: "ku9",
+        name: "クク・ブロッサム",
         birthday: "9月9日 (19歳)",
         position: "サブボーカル / サブラッパー",
         image: "./images/ku9.png"
