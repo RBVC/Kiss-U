12a1ccda90436ae4ee38b0de7ead8ce21a5813f3
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-09 01:47:05 +0900

    up

--- data.js
+++ data.js
@@ -1,11 +1,13 @@
+// --- メンバーデータ ---
 const memberData = {
     "1chika": { codename: "1chika", name: "イチカ・ハイビスカス", birthday: "1月5日 (22歳)", position: "メインボーカル", image: "./images/1chika.png" },
-    "3tuba": { codename: "3tuba", name: "ミツハ・トリフォリウム", birthday: "3月8日 (18歳)", position: "マンネ / メインダンサー / メインラッパー", image: "./images/3tuba.png" },
+    "3tuba": { codename: "3tuba", name: "ミツバ・トリフォリウム", birthday: "3月8日 (18歳)", position: "マンネ / メインダンサー / メインラッパー", image: "./images/3tuba.png" },
     "5ma": { codename: "5ma", name: "ゴマ・フリージア", birthday: "5月20日 (25歳)", position: "リーダー / リードボーカル", image: "./images/5ma.png" },
     "na7o": { codename: "na7o", name: "ナナオ・ピクシーデライト", birthday: "7月14日 (23歳)", position: "リードボーカル / リードラッパー", image: "./images/na7o.png" },
     "ku9": { codename: "ku9", name: "クク・ブロッサム", birthday: "9月9日 (19歳)", position: "サブボーカル / サブラッパー", image: "./images/ku9.png" }
 };
 
+// --- ディスコグラフィデータ ---
 const discoData = {
     "fly-by": {
         title: "1st EP \"FLY-BY\"",
@@ -15,6 +17,34 @@ const discoData = {
             { num: "01", name: "Yum", file: "./music/fly-by/01_yum.m4a" },
             { num: "02", name: "MVSK", file: "./music/fly-by/02_mvsk.m4a" },
             { num: "03", name: "Bubble Gum", file: "./music/fly-by/03_bubble_gum.m4a" }
+        ]
+    },
+    "the-aend": {
+        title: "2nd EP \"the a(e)nd\"",
+        release: "2026.08.20",
+        jacket: "https://images.genius.com/b47591b4de032d8e9ffaecba356b43db.1000x1000x1.png",
+        tracks: [
+            { num: "01", name: "BabyDon'tCry=BreakingTheLove", file: "./music/theaend/01_babydontcry.m4a" },
+            { num: "02", name: "Zigy Zigy", file: "./music/theaend/02_zigy_zigy.m4a" },
+            { num: "03", name: "Lucky=One", file: "./music/theaend/03_lucky_one.m4a" }
+        ]
+    },
+    "awe": {
+        title: "3rd EP \"AWE\"",
+        release: "2026.11.15",
+        jacket: "https://images.genius.com/8fa0c7a2791bfb16e25e4e23f4dd7a45.1000x563x1.jpg",
+        tracks: [
+            { num: "01", name: "IYKYK", file: "./music/awe/01_iykyk.m4a" },
+            { num: "02", name: "Something Ain't Right", file: "./music/awe/02_something_aint_right.m4a" },
+            { num: "03", name: "Is This Love", file: "./music/awe/03_is_this_love.m4a" }
+        ]
+    },
+    "come-over": {
+        title: "Digital Single \"Come Over\"",
+        release: "2027.01.05",
+        jacket: "https://ih1.redbubble.net/image.5809651142.3212/fposter,small,wall_texture,square_product,600x600.jpg",
+        tracks: [
+            { num: "01", name: "Come Over", file: "./music/come_over.m4a" }
         ]
     }
 };
