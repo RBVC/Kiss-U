142c117b653431caa19140ce907f0162626e918e
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-09 22:15:01 +0900

    Add files via upload

---
+++ common.js
@@ -0,0 +1,28 @@
+// 全ページ共通のヘッダーを注入
+const headerArea = document.getElementById('common-header');
+if (headerArea) {
+    headerArea.innerHTML = `
+    <header>
+        <a href="index.html" class="logo">Kiss-U</a>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="index.html#news">News</a></li>
+                <li><a href="index.html#profile">Profile</a></li>
+                <li><a href="index.html#discography">Discography</a></li>
+            </ul>
+        </nav>
+    </header>
+    `;
+}
+
+// 全ページ共通のフッターを注入
+const footerArea = document.getElementById('common-footer');
+if (footerArea) {
+    footerArea.innerHTML = `
+    <footer>
+        <div class="copyright">
+            Copyright &copy; Kiss-U JAPAN. All Rights Reserved.
+        </div>
+    </footer>
+    `;
+}
