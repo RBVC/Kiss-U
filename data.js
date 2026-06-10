1659f00367dd272ddc06504bd301323b94001115
RBVC <sacra.motimoti+RVC@gmail.com>
2026-06-10 09:21:10 +0900

    UP

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+// --- メンバーデータ ---
 const memberData = {
     "1chika": { codename: "1chika", name: "イチカ・ハイビスカス", birthday: "1月5日 (22歳)", position: "メインボーカル", image: "./images/1chika.png" },
     "3tuba": { codename: "3tuba", name: "ミツバ・トリフォリウム", birthday: "3月8日 (18歳)", position: "マンネ / メインダンサー / メインラッパー", image: "./images/3tuba.png" },
@@ -6,6 +7,7 @@ const memberData = {
     "ku9": { codename: "ku9", name: "クク・ブロッサム", birthday: "9月9日 (19歳)", position: "サブボーカル / サブラッパー", image: "./images/ku9.png" }
 };
 
+// --- ニュースデータ ---
 const newsData = [
     { date: "2027.01.05", text: "Digital Single \"Come Over\" リリース決定", link: "discography.html?id=come-over" },
     { date: "2026.11.15", text: "3rd EP \"AWE\" リリース決定", link: "discography.html?id=awe" },
@@ -14,9 +16,42 @@ const newsData = [
     { date: "2026.06.08", text: "Kiss-U Official Site オープン", link: "#" }
 ];
 
+// --- ディスコグラフィデータ (曲名リニューアル) ---
 const discoData = {
-    "come-over": { title: "Digital Single \"Come Over\"", release: "2027.01.05", jacket: "https://ih1.redbubble.net/image.5809651142.3212/fposter,small,wall_texture,square_product,600x600.jpg", tracks: [{ num: "01", name: "Come Over", file: "./music/come_over.m4a" }] },
-    "awe": { title: "3rd EP \"AWE\"", release: "2026.11.15", jacket: "https://img.imageimg.net/artist/xg/img/product_1037894.png", tracks: [{ num: "01", name: "IYKYK", file: "./music/awe/01_iykyk.m4a" }, { num: "02", name: "Something Ain't Right", file: "./music/awe/02_something_aint_right.m4a" }, { num: "03", name: "Is This Love", file: "./music/awe/03_is_this_love.m4a" }] },
-    "the-aend": { title: "2nd EP \"the a(e)nd\"", release: "2026.08.20", jacket: "https://images.genius.com/b47591b4de032d8e9ffaecba356b43db.1000x1000x1.png", tracks: [{ num: "01", name: "BabyDon'tCry=BreakingTheLove", file: "./music/theaend/01_babydontcry.m4a" }, { num: "02", name: "Zigy Zigy", file: "./music/theaend/02_zigy_zigy.m4a" }, { num: "03", name: "Lucky=One", file: "./music/theaend/03_lucky_one.m4a" }] },
-    "fly-by": { title: "1st EP \"FLY-BY\"", release: "2026.06.09", jacket: "https://images.genius.com/a16644bd336d84437e195572f9ae309f.1000x1000x1.png", tracks: [{ num: "01", name: "Yum", file: "./music/fly-by/01_yum.m4a" }, { num: "02", name: "MVSK", file: "./music/fly-by/02_mvsk.m4a" }, { num: "03", name: "Bubble Gum", file: "./music/fly-by/03_bubble_gum.m4a" }] }
+    "come-over": { 
+        title: "Digital Single \"Come Over\"", 
+        release: "2027.01.05", 
+        jacket: "https://ih1.redbubble.net/image.5809651142.3212/fposter,small,wall_texture,square_product,600x600.jpg", 
+        tracks: [{ num: "01", name: "Come Over", file: "./music/come_over.m4a" }] 
+    },
+    "awe": { 
+        title: "3rd EP \"AWE\"", 
+        release: "2026.11.15", 
+        jacket: "https://img.imageimg.net/artist/xg/img/product_1037894.png", 
+        tracks: [
+            { num: "01", name: "IYKYK", file: "./music/awe/01_iykyk.m4a" }, 
+            { num: "02", name: "SOMETHING AIN'T RIGHT", file: "./music/awe/02_something_aint_right.m4a" }, 
+            { num: "03", name: "IS THIS LOVE", file: "./music/awe/03_is_this_love.m4a" }
+        ] 
+    },
+    "the-aend": { 
+        title: "2nd EP \"the a(e)nd\"", 
+        release: "2026.08.20", 
+        jacket: "https://images.genius.com/b47591b4de032d8e9ffaecba356b43db.1000x1000x1.png", 
+        tracks: [
+            { num: "01", name: "BabyDon'tCry=BreakingTheLove", file: "./music/theaend/01_babydontcry.m4a" }, 
+            { num: "02", name: "zigy=zigy", file: "./music/theaend/02_zigy_zigy.m4a" }, 
+            { num: "03", name: "Lucky=One", file: "./music/theaend/03_lucky_one.m4a" }
+        ] 
+    },
+    "fly-by": { 
+        title: "1st EP \"FLY-BY\"", 
+        release: "2026.06.09", 
+        jacket: "https://images.genius.com/a16644bd336d84437e195572f9ae309f.1000x1000x1.png", 
+        tracks: [
+            { num: "01", name: "Yum", file: "./music/fly-by/01_yum.m4a" }, 
+            { num: "02", name: "MVSK", file: "./music/fly-by/02_mvsk.m4a" }, 
+            { num: "03", name: "BUBBLE GUM", file: "./music/fly-by/03_bubble_gum.m4a" }
+        ] 
+    }
 };
